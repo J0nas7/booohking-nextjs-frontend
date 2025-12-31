d3149186debce3d46dc8386df148a894e53c98a1
J0nas7 <jonas.sorensen.93dk@gmail.com>
2025-12-31 10:16:33 +0100

    More Terraform variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,39 @@
+# Root module variables for GitHub Actions deployment
+
+variable "container_name" {
+  type        = string
+  description = "Name of the Scaleway container"
+}
+
 variable "registry_image" {
   type        = string
   description = "Docker image to deploy"
+}
+
+variable "scaleway_access_key" {
+  type        = string
+  description = "Scaleway API access key"
+  sensitive   = true
+}
+
+variable "scaleway_secret_key" {
+  type        = string
+  description = "Scaleway API secret key"
+  sensitive   = true
+}
+
+variable "scaleway_project_id" {
+  type        = string
+  description = "Scaleway project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "Scaleway region"
+  default     = "fr-par"
+}
+
+variable "namespace_name" {
+  type        = string
+  description = "Scaleway container namespace name"
 }
